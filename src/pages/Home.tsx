a03fd9d04007531c536a706fa6d0eccf92d568ac
praxodigital-netizen <praxodigital@gmail.com>
2026-03-25 14:58:29 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -304,6 +304,9 @@ export const Home: React.FC = () => {
     Cancel anytime • No hidden charges
   </p>
 </div>
+</div>
+</div>
+</section>
 
       {/* Who is this for? */}
       <section id="features" className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-24 border-t border-white/10">
