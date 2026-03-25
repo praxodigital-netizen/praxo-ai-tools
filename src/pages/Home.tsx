134ec351609990e5d08063e94da31865d28dc875
praxodigital-netizen <praxodigital@gmail.com>
2026-03-25 18:49:20 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRazorpay } from '../hooks/useRazorpay';
 import { useUsageStore } from '../store/usage';
 import { LoginModal } from '../components/LoginModal';
